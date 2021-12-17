c5ff7004931f5ecf1d8342e2a46494efccf98e9a
ranjan-nitish <Nitish.Ranjan18@st.niituniversity.in>
2021-12-17 19:43:44 +0530

    Selenium added

---
+++ TrafficSignRecognition/selenium/test_tsr.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+
+
+
+
+class TestTSR():
+
+    @pytest.fixture()
+    def setup(self):
+        #self.driver = webdriver.Chrome=webdriver.Chrome(ChromeDriverManager().install())
+        self.driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+        self.driver.maximize_window()
+        yield
+        self.driver.close()
+
+
+    
+
+    
+    def test_homePageTitle(self,setup):
+        self.driver.get("http://127.0.0.1:8000/")
+        assert self.driver.title=="Traffic sign Detection"
+
+    
+
+        
